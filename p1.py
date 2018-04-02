dfd4edce57daa9d5a43450e634c506098071e7b5
Neil <nilnil47@gmail.com>
2018-04-03 00:35:33 +0300

    fix the repo

--- p1.py
+++
@@ -1,8 +0,0 @@
-max_number = 1000
-
-mul_of_3 = list(range(0, max_number, 3))
-mul_of_5 = list(range(0, max_number, 5))
-mul_of_15 = list(range(0, max_number, 3 * 5))
-
-s = sum(mul_of_3) + sum(mul_of_5) - sum(mul_of_15)
-print(s)
